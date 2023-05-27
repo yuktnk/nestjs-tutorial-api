be654e432aa5f0f8dc2d17c79f6c2b821aad325a
yuktnk <yuki0wl@gmail.com>
2023-05-27 12:16:41 +0900

    Read, Createのみ作成

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostType } from './post.interface';
 
 @Controller('posts')
 export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
   @Get()
   findAll() {
-    return 'get all posts';
+    return this.postsService;
+  }
+
+  @Post()
+  create(@Body() post: PostType): void {
+    this.postsService.create(post);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PostType } from './post.interface';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  private readonly posts: PostType[] = [];
+
+  findAll(): PostType[] {
+    return this.posts;
+  }
+
+  create(post: PostType) {
+    this.posts.push(post);
+  }
+}
